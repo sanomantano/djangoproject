3eb02db90312c36f921a2f03b7467a262c33e2a8
falcox456 <mansursooronkulov@sris.edu.kg>
2025-04-16 18:03:05 +0600

    first commit

--- apps/gifts/models.py
+++ apps/gifts/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Feedback(models.Model):
+    rate = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Отзыв'
+        verbose_name_plural = 'Отзывы'
